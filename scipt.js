e6284dbb490424c946e857e70f613e1eacd76927
Paola <paolabitalo@gmail.com>
2025-04-03 23:47:54 +0000

    final draft

---
+++ scipt.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    lax.setup()
+
+    const updateLax = () => {
+        lax.update(window.scrollY)
+        window.requestAnimationFrame(updateLax)
+    }
+
+    window.requestAnimationFrame(updateLax)
+}
